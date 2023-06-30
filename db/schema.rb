9b0545a006adc1c705456ca6c2009941acfda5a8
ogurayusuke <oguyu2021@gmail.com>
2023-06-30 16:07:29 +0900

    ブログ一覧機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2023_06_29_082321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -24,10 +31,4 @@ ActiveRecord::Schema.define(version: 2023_06_29_082321) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 end
